45fc44530eda31eda6a80062da4a3d82bb101387
azerata <smedemarkhammer@gmail.com>
2022-10-13 13:17:04 +0200

    Cleaning up redundant stuff

--- pyt/viterbi.c
+++ pyt/viterbi.c
@@ -2,13 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define STATES states
-#define EMITS emits
-#define N n
-
-#define T(I, J) trans_[(I)*STATES + (J)]
-#define E(I, J) emits_[(I)*EMITS + (J)]
-#define V(I, J) vert_[(I)*STATES + (J)]
+#define T(I, J) trans_[(I)*states + (J)]
+#define E(I, J) emits_[(I)*emits + (J)]
+#define V(I, J) vert_[(I)*states + (J)]
 
 double max(double a, double b)
 {
@@ -21,12 +17,11 @@ void viterbi(const double *pi, const double *trans_, const double *emits_, doubl
                             emission matrix [emits_],
                             output matrix [vert_],
                             output optimal path [opt_p]
-                            input sequence [x],)
-                            # of [states] [emits], [input_length]
+                            input sequence [x],
+                            # of [states], [emits], [input_length])
                             */
 {
     // Viterbi algorithm
-    printf("%f\n", *vert_);
     for (int j = 0; j < states; j++)
     {
         V(0, j) = E(j, x[0]) + pi[j];
@@ -36,7 +31,7 @@ void viterbi(const double *pi, const double *trans_, const double *emits_, doubl
     {
         for (int j = 0; j < states; j++)
         {
-            double best = -10000.0;
+            double best = -INFINITY;
             for (int k = 0; k < states; k++)
             {
                 best = max(best, V(i - 1, k) + T(k, j));
@@ -45,7 +40,7 @@ void viterbi(const double *pi, const double *trans_, const double *emits_, doubl
         }
     }
     // Backtracking now included
-    double best = -1000.0;
+    double best = -INFINITY;
     int end = 0;
     for (int j = 0; j < states; j++)
     {
@@ -60,10 +55,6 @@ void viterbi(const double *pi, const double *trans_, const double *emits_, doubl
         for (int j = 0; j < states; j++)
         {
             opt_p[i] = (V(i, j) + T(j, end) + E(end, x[i + 1]) == target) ? j : opt_p[i];
-            printf("%lf, %lf, %lf ==? %lf, %d\n", T(j, end), V(i, j),
-                   V(i, j) + T(j, end) + E(end, x[i + 1]),
-                   target, opt_p[i]);
         }
-        printf("\n");
     }
 }
--- pyt/viterbi.py
+++ pyt/viterbi.py
@@ -1,8 +1,11 @@
 import numpy as np
 from ctypes import CDLL, c_double, c_int
 
-lib = CDLL("./viterbi.so")
+
+lib = CDLL("./viterbi.so")  # load library containing c functions
 viterbi = lib.viterbi
+
+# Set in and output types:
 viterbi.restype = None
 viterbi.argtypes = [np.ctypeslib.ndpointer(c_double),
                     np.ctypeslib.ndpointer(c_double),
@@ -16,38 +19,23 @@ input format:   viterbi(initial probability [pi],
                         transition matrix [trans_],
                         emission matrix [emits_],
                         output matrix [vert_],
-                        input sequence [x]  ) 
-                        # of [states] [emits], [input_length]
+                        output optimal path [out_p]
+                        input sequence [x],
+                        # of [states] [emits], [input_length])
 '''
-x = np.array([1, 2, 0, 1], dtype=c_int)
-
-pi = np.log(np.array([[1],
-                      [0]], dtype=c_double))
-
-t = np.log(np.array([[0.7, 0.3],
-                     [0.2, 0.8]], dtype=c_double))
-
-e = np.log(np.array([[0.1, 0.9],
-                    [0.7, 0.3]], dtype=c_double))
-v = np.zeros((x.size, pi.size), dtype=c_double)
-
-p = np.zeros(x.shape, dtype=c_int)
-# print(v)
-#viterbi(pi, t, e, v, p, x, c_int(pi.size), c_int(e.shape[0]), c_int(x.size))
-# print(v)
-# print(p)
-
 inp = np.array([1, 1, 1, 1, 0, 0, 0, 0, 0], dtype=c_int)
 
+
+# try with test Data:
 init_probs_3_state = np.log(np.array(
     [0.00, 0.10, 0.00],
     dtype=c_double
 ))
 
 trans_probs_3_state = np.log(np.array([
-    [0.00, 1.00, 0.00],
-    [0.00, 0.00, 1.00],
-    [1.00, 0.00, 0.00],
+    [0.00, 0.05, 0.95],
+    [0.20, 0.00, 0.80],
+    [0.95, 0.05, 0.00],
 ], dtype=c_double))
 
 emission_probs_3_state = np.log(np.array([
@@ -57,14 +45,11 @@ emission_probs_3_state = np.log(np.array([
     [0.20, 0.40, 0.30, 0.10],
 ], dtype=c_double))
 
-sanity_check = np.log(np.array([[0.40, 0.25, 0.20],
-                                [0.15, 0.25, 0.40],
-                                [0.20, 0.25, 0.30],
-                                [0.25, 0.25, 0.10]], dtype=c_double))
-
 out_p = np.zeros(inp.shape, dtype=c_int)
 v_table = np.zeros((inp.size, init_probs_3_state.size), dtype=c_double)
 
+
+# run!
 viterbi(init_probs_3_state, trans_probs_3_state, emission_probs_3_state,
         v_table, out_p, inp, c_int(3), c_int(4), c_int(inp.size))
 print(v_table)
